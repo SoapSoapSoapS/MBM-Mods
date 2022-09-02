a092a8e20776cefc8b71248073a55a5ff234bce9
SoapBoxHero <fernandoq@outlook.com>
2022-09-01 20:10:16 -0400

    A ton of changes

---
+++ mbmModdingTools/ConfigInfo.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx.Configuration;
+
+namespace MbmModdingTools
+{
+    public struct ConfigInfo<T>
+    {
+        public string Section;
+        public string Name;
+        public string Description;
+        public T DefaultValue;
+        public AcceptableValueBase? AcceptableValues;
+    }
+
+    public static class ConfigExtensions
+    {
+        public static ConfigEntry<T> Bind<T>(this ConfigFile file, ConfigInfo<T> info)
+        {
+            return file.Bind(new ConfigDefinition(info.Section, info.Name), info.DefaultValue, new ConfigDescription(info.Description, info.AcceptableValues));
+        }
+    }
+
+}
