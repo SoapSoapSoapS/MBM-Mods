58ac410c22b5c2878e8288c3ff416a0f0d6abadd
Soap <soapsoapsoaps@outlook.com>
2023-02-25 23:52:48 -0500

    V2

---
+++ Restless/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Unity.Mono;
+
+namespace Restless2;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
---
+++ Restless/PluginInfo.cs
@@ -0,0 +1,5 @@
+
+public static class PluginInfo {
+    public const string PLUGIN_GUID = "soapsoapsoaps.plugins.restless";
+    public const string PLUGIN_NAME = "Restless";
+}
