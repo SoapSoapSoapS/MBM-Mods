42efe10b102541fac58b72fe6606ee048437ad44
Soap <soapsoapsoaps@outlook.com>
2023-02-27 19:55:49 -0500

    Update plugin template

--- Src/Boilerplate/Plugin.cs
+++ Src/Boilerplate/Plugin.cs
@@ -6,6 +6,19 @@ namespace Boilerplate;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    /// <summary>
+    /// Mod log instance
+    /// </summary>
+    public static BepInEx.Logging.ManualLogSource? log;
+
+    /// <summary>
+    /// Initialize logger.
+    /// </summary>
+    public Plugin()
+    {
+        log = Logger;
+    }
+    
     private void Awake()
     {
         // Plugin startup logic
